0ed9f6c3e1bb16b4a98394e3a94e29b4cbdb8f3a
Akshita <Akshita@DESKTOP-VQDS947>
2021-02-09 15:49:17 -0800

    java.util packages for scanner class and Random were imported Akshita Sreenivasulu February 8, 2020 991592661

--- PickACard_starterCode_ICE1 (2)/PickACard/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ PickACard_starterCode_ICE1 (2)/PickACard/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -4,6 +4,8 @@
  * 991592661
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
