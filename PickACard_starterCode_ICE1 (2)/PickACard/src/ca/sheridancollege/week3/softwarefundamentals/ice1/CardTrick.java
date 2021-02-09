2e3dfb22d45c80275169ac506a79767c74a9128f
Akshitha <Akshitha@DESKTOP-VQDS947>
2021-02-09 15:26:36 -0800

    Name, date and student ID number added to code Akshita Sreenivasulu February 8, 2020 991592661

--- PickACard_starterCode_ICE1 (2)/PickACard/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ PickACard_starterCode_ICE1 (2)/PickACard/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Akshita Sreenivasulu
+ * February 9, 2020
+ * 991592661
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
